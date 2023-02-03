1de1045a5484a558b0d49074d98f0a16f290fbcc
hiren <hirenpatel.ict19@gmail.com>
2023-02-03 18:32:25 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const About = () => {
+    let fonts={
+        font:" font-family: 'Cascadia Code', Consolas,'Courier New', monospace , FiraCode",
+    }
+    return (
+        <div>
+            This is an about component 
+            <p style={fonts}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum corporis odio, nesciunt sed eveniet, nemo amet, dignissimos magnam itaque ipsam possimus. Inventore vitae, perferendis harum quas cupiditate adipisci repellendus tempore asperiores at consequatur quos?</p>
+        </div>
+    )
+    
+}
+
+
